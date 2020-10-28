a7c53471c4d3745c4ec30e74262f53ad171730d0
Anton Chyvikov <chivikovanton@gmail.com>
2020-10-28 13:43:56 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let popupLingvaA1A2 = new PopUp({
+    openBtn: 'showModal-lingvaA1A2',
+    container: 'popupContainer__lingvaA1A2',
+    content: ` <img class="course__img w-100" src="img/diploma.jpg" alt="certificate">`,
+    maskColor: '#1e2939',
+    maskOpacity: '0.7',
+    })
+
+    let popupItNationCourse = new PopUp({
+        openBtn: 'showModal-itNationCourse',
+        container: 'popupContainer__itNationCourse',
+        content: ` <img class="course__img w-100" src="img/certificate.jpg" alt="certificate">`,
+        maskColor: '#1e2939',
+        maskOpacity: '0.7',
+        })
+
+        let popupitBootcamp = new PopUp({
+            openBtn: 'showModal-itBootcamp',
+            container: 'popupContainer__itBootcamp',
+            content: ` <img class="course__img w-100" src="img/bootcamp.jpg" alt="certificate">`,
+            maskColor: '#1e2939',
+            maskOpacity: '0.7',
+            })
