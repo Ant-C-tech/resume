96e7126ecccd4a626048c788af5efe88edbd4b7f
Chyvikov-Anton <chivikovanton@gmail.com>
2020-11-18 12:17:44 +0200

    Add King's College London Education

--- js/app.js
+++ js/app.js
@@ -4,20 +4,36 @@ let popupLingvaA1A2 = new PopUp({
     content: ` <img class="course__img w-100" src="img/diploma.jpg" alt="certificate">`,
     maskColor: '#1e2939',
     maskOpacity: '0.7',
-    })
+})
 
-    let popupItNationCourse = new PopUp({
-        openBtn: 'showModal-itNationCourse',
-        container: 'popupContainer__itNationCourse',
-        content: ` <img class="course__img w-100" src="img/certificate.jpg" alt="certificate">`,
-        maskColor: '#1e2939',
-        maskOpacity: '0.7',
-        })
+let popupItNationCourse = new PopUp({
+    openBtn: 'showModal-itNationCourse',
+    container: 'popupContainer__itNationCourse',
+    content: ` <img class="course__img w-100" src="img/certificate.jpg" alt="certificate">`,
+    maskColor: '#1e2939',
+    maskOpacity: '0.7',
+})
 
-        let popupitBootcamp = new PopUp({
-            openBtn: 'showModal-itBootcamp',
-            container: 'popupContainer__itBootcamp',
-            content: ` <img class="course__img w-100" src="img/bootcamp.jpg" alt="certificate">`,
-            maskColor: '#1e2939',
-            maskOpacity: '0.7',
-            })
+let popupitBootcamp = new PopUp({
+    openBtn: 'showModal-itBootcamp',
+    container: 'popupContainer__itBootcamp',
+    content: ` <img class="course__img w-100" src="img/bootcamp.jpg" alt="certificate">`,
+    maskColor: '#1e2939',
+    maskOpacity: '0.7',
+})
+
+let popupKkl1 = new PopUp({
+    openBtn: 'showModal-kkl-1',
+    container: 'popupContainer__kkl-1',
+    content: ` <img class="course__img w-100" src="img/basic-english-pre-intermediate_certificate_of_achievement_1.jpg" alt="certificate">`,
+    maskColor: '#1e2939',
+    maskOpacity: '0.7',
+})
+
+let popupKkl2 = new PopUp({
+    openBtn: 'showModal-kkl-2',
+    container: 'popupContainer__kkl-2',
+    content: ` <img class="course__img w-100" src="img/basic-english-pre-intermediate_certificate_of_achievement_2.jpg" alt="certificate">`,
+    maskColor: '#1e2939',
+    maskOpacity: '0.7',
+})
