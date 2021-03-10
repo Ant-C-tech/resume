34b16496dcb603b941756d8a98ef51d741ec6ee3
Chyvikov-Anton <chivikovanton@gmail.com>
2021-03-10 14:42:02 +0200

    Add test project without mobile optimization

---
+++ js/project.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let popupCinemaApp1 = new PopUp({
+  openBtn: "showModal-cinemaApp-1",
+  container: "popupContainer__cinemaApp-1",
+  content: ` <img class="course__img w-100" src="./img/projects/22/proj22.jpg" alt="screenshot of cinema application">`,
+  maskColor: "#1e2939",
+  maskOpacity: "0.7",
+});
+
+let popupCinemaApp2 = new PopUp({
+  openBtn: "showModal-cinemaApp-2",
+  container: "popupContainer__cinemaApp-2",
+  content: ` <img class="course__img w-100" src="./img/projects/22/proj22-2.jpg" alt="screenshot of cinema application">`,
+  maskColor: "#1e2939",
+  maskOpacity: "0.7",
+});
