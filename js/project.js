68c66165c14de04a43cb133f77451f31848d3f07
Chyvikov-Anton <chivikovanton@gmail.com>
2021-03-12 17:45:39 +0200

    Add project #19

--- js/project.js
+++ js/project.js
@@ -3,7 +3,7 @@
 let popupCinemaApp1 = new PopUp({
   openBtn: "showModal-cinemaApp-1",
   container: "popupContainer__cinemaApp-1",
-  content: ` <img class="course__img w-100" src="./img/projects/22/proj22.jpg" alt="screenshot of cinema application">`,
+  content: ` <img class="course__img w-100" src="./img/projects/22/proj22.jpg" alt="screenshot of Cinema Application">`,
   maskColor: "#1e2939",
   maskOpacity: "0.7",
 });
@@ -11,7 +11,7 @@ let popupCinemaApp1 = new PopUp({
 let popupCinemaApp2 = new PopUp({
   openBtn: "showModal-cinemaApp-2",
   container: "popupContainer__cinemaApp-2",
-  content: ` <img class="course__img w-100" src="./img/projects/22/proj22-2.jpg" alt="screenshot of cinema application">`,
+  content: ` <img class="course__img w-100" src="./img/projects/22/proj22-2.jpg" alt="screenshot of Cinema Application">`,
   maskColor: "#1e2939",
   maskOpacity: "0.7",
 });
@@ -19,7 +19,7 @@ let popupCinemaApp2 = new PopUp({
 let popupFriendsApp1 = new PopUp({
   openBtn: "showModal-friendsApp-1",
   container: "popupContainer__friendsApp-1",
-  content: ` <img class="course__img w-100" src="./img/projects/21/proj21.jpg" alt="screenshot of cinema application">`,
+  content: ` <img class="course__img w-100" src="./img/projects/21/proj21.jpg" alt="screenshot of Friends App">`,
   maskColor: "#1e2939",
   maskOpacity: "0.7",
 });
@@ -27,7 +27,7 @@ let popupFriendsApp1 = new PopUp({
 let popupFriendsApp2 = new PopUp({
   openBtn: "showModal-friendsApp-2",
   container: "popupContainer__friendsApp-2",
-  content: ` <img class="course__img w-100" src="./img/projects/21/proj21-2.jpg" alt="screenshot of cinema application">`,
+  content: ` <img class="course__img w-100" src="./img/projects/21/proj21-2.jpg" alt="screenshot of Friends App">`,
   maskColor: "#1e2939",
   maskOpacity: "0.7",
 });
@@ -36,6 +36,22 @@ let popupFroggerGame = new PopUp({
   openBtn: "showModal-froggerGame",
   container: "popupContainer__froggerGame",
   content: ` <img class="course__img w-100" src="./img/projects/20/proj20.jpg" alt="screenshot of Frogger Game">`,
+  maskColor: "#1e2939",
+  maskOpacity: "0.7",
+});
+
+let popupMemoryPairGame1 = new PopUp({
+  openBtn: "showModal-memoryPairGame-1",
+  container: "popupContainer__memoryPairGame-1",
+  content: ` <img class="course__img w-100" src="./img/projects/19/proj19.jpg" alt="screenshot of Memory-Pair Game">`,
+  maskColor: "#1e2939",
+  maskOpacity: "0.7",
+});
+
+let popupMemoryPairGame2 = new PopUp({
+  openBtn: "showModal-memoryPairGame-2",
+  container: "popupContainer__memoryPairGame-2",
+  content: `<img class="course__img w-100" src="./img/projects/19/proj19-2.jpg" alt="screenshot of Memory-Pair Game">`,
   maskColor: "#1e2939",
   maskOpacity: "0.7",
 });
